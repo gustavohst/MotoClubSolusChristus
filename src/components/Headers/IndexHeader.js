ff17cc655e1b22b88cfda70f49c2dca690766af9
Gustavo Tenório <gustavohst@msn.com>
2019-09-12 21:21:38 -0300

    add logo index header, title and remove a few tags

--- src/components/Headers/IndexHeader.js
+++ src/components/Headers/IndexHeader.js
@@ -31,24 +31,24 @@ function IndexHeader() {
         className="page-header section-dark"
         style={{
           backgroundImage:
-            "url(" + require("assets/img/antoine-barres.jpg") + ")"
+            "url(" + require("assets/img/LogoSolusChristus.jpg") + ")"
         }}
       >
         <div className="filter" />
         <div className="content-center">
           <Container>
             <div className="title-brand">
-              <h1 className="presentation-title">Paper Kit React</h1>
+              {/* <h1 className="presentation-title">Solus Christus MCC</h1> */}
               <div className="fog-low">
-                <img alt="..." src={require("assets/img/fog-low.png")} />
+                {/* <img alt="..." src={require("assets/img/fog-low.png")} /> */}
               </div>
               <div className="fog-low right">
-                <img alt="..." src={require("assets/img/fog-low.png")} />
+                {/* <img alt="..." src={require("assets/img/fog-low.png")} /> */}
               </div>
             </div>
-            <h2 className="presentation-subtitle text-center">
-              Make your mark with a Free Bootstrap 4 (Reactstrap) UI Kit!
-            </h2>
+            {/* <h2 className="presentation-subtitle text-center"> */}
+              {/* Moto Clube Cristão  */}
+            {/* </h2> */}
           </Container>
         </div>
         <div
--- src/components/Navbars/IndexNavbar.js
+++ src/components/Navbars/IndexNavbar.js
@@ -71,7 +71,7 @@ function IndexNavbar() {
             target="_blank"
             title="Coded by Creative Tim"
           >
-            Paper Kit React
+            Solus Christus MCC
           </NavbarBrand>
           <button
             aria-expanded={navbarCollapse}
